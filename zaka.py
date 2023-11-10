a66d43a704017de7fa9fb55ece67f974a64608e6
chyngyz <chyngyzbatyrov170@gmail.com>
2023-11-10 12:32:28 +0600

    dassa

---
+++ zaka.py
@@ -0,0 +1,20 @@
+
+
+a = 3
+b = 2
+c = 1
+d = 5
+print((a < b and a < b) or (a > b or a >= c) and (c <= a and c <= b))
+
+
+
+temp = (input("Аба ырайын киргиз: "))
+
+if temp >= 20:
+    print("фудболканды кий, кун ысык!")
+
+if temp <= 19 and temp >= 1:
+    print("куртканды кий, кун суук!")
+
+if temp <= 0:
+    print("сыртка чыкпай эле кой ,олосун!")
